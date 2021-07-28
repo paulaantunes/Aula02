073e73a5ef13c6252e7b068adcbe9a5f1e927b81
Paula Antunes <paula-antunes@outlook.com>
2021-07-28 20:45:22 -0300

    Implementado o if simples

--- src/DesvioSimples.java
+++ src/DesvioSimples.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class DesvioSimples {
 
 	public static void main(String[] args) {
+		
 		/*Uma ONG criou um jogo para alertar as pessoas sobre os riscos das drogas.
-		 *Por se tratar de um assunto sensível, ficou decidido que a idade mínima para jogar é de 12 anos.
-		 *Sua função é criar um programa que receba a idade do usuário e exiba a mensagem "Você pode jogar" caso ele tenha 12 anos ou mais. */
+		 *Por se tratar de um assunto sensivel, ficou decidido que a idade minima para jogar eh de 12 anos.
+		 *Sua funcao eh criar um programa que receba a idade do usuario e exiba a mensagem "Voce pode jogar" caso ele tenha 12 anos ou mais. */
 		
 		Scanner leitor = new Scanner(System.in);
 		int idade;
 		System.out.println("Por favor, digite a sua idade!");
 		idade = leitor.nextInt();
 		System.out.println("Deixe-me verificar sua idade...");
-		/*Aqui devemos criar uma solução para verificar a idade do usuário*/
 		
+		/*Aqui devemos criar uma solucao para verificar a idade do usuario*/
+		
+		if (idade>=12) {
+			System.out.println("VocÃª pode jogar");		}
+			
 		leitor.close();
 
 	}
