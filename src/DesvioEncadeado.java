36ebe449028f41eb268966a46f9cebe0e561c2e6
Paula Antunes <paula-antunes@outlook.com>
2021-07-28 21:09:52 -0300

    Implementado o if encadeado

--- src/DesvioEncadeado.java
+++ src/DesvioEncadeado.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class DesvioEncadeado {
 
 	public static void main(String[] args) {
-		/*Uma ONG resolveu prestar um serviço bem diferente: ela oferece vans para buscar pessoas com qualquer tipo de dificuldade de locomoção para poderem votar.
-		 * Para evitar problemas no momento do embarque, porém, você foi convidado a criar um programa que valide a idade dos passageiros: caso tenham 16 anos ou menos, não podem votar (e nem embarcar). Caso tenham entre 16 anos e 18 incompletos, podem optar por votar ou não. Caso tenham mais de 18 anos, devem votar obrigatoriamente.
+		/*Uma ONG resolveu prestar um serviï¿½o bem diferente: ela oferece vans para buscar pessoas com qualquer tipo de dificuldade de locomocao para poderem votar.
+		 * Para evitar problemas no momento do embarque, porem, voce foi convidado a criar um programa que valide a idade dos passageiros: caso tenham menos de 16 anos, nao podem votar (e nem embarcar). Caso tenham entre 16 anos e 18 incompletos, podem optar por votar ou nao. Caso tenham mais de 18 anos, devem votar obrigatoriamente.
 		 * Crie um programa que receba a idade dos passageiros em potencial e indique se podem embarcar e votar.*/
 		
 		Scanner leitor = new Scanner(System.in);
@@ -12,8 +12,19 @@ public class DesvioEncadeado {
 		System.out.println("Por favor, digite a idade do passageiro.");
 		idade = leitor.nextInt();
 	
-		/*Aqui devemos criar uma solução para realizar as verificações.*/
+		/*Aqui devemos criar uma soluï¿½ï¿½o para realizar as verificaï¿½ï¿½es.*/
 		
+		if (idade<16) {
+			System.out.println("O passageiro nÃ£o pode votar e nem embarcar");
+			
+		}else {
+			if (idade>=18) {
+				System.out.println("O passageiro Ã© obrigado a votar e pode embarcar");
+				
+			}else {
+				System.out.println("O passageiro pode votar e pode embarcar");
+			}
+		}
 		
 	
 		leitor.close();
