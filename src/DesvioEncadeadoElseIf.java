f82a3642c03a0754a72efa43abd434513b08ee12
Paula Antunes <paula-antunes@outlook.com>
2021-08-07 16:56:29 -0300

    Atualização de baixo impacto

--- src/DesvioEncadeadoElseIf.java
+++ src/DesvioEncadeadoElseIf.java
@@ -4,11 +4,19 @@ public class DesvioEncadeadoElseIf {
 
 	public static void main(String[] args) {
 		/*Uma ONG resolveu prestar um servi�o bem diferente: ela oferece vans para buscar pessoas com qualquer tipo de dificuldade de locomocao para poderem votar.
-		 * Para evitar problemas no momento do embarque, porem, voce foi convidado a criar um programa que valide a idade dos passageiros: caso tenham menos de 16 anos, nao podem votar (e nem embarcar). Caso tenham entre 16 anos e 18 incompletos, podem optar por votar ou nao. Caso tenham mais de 18 anos, devem votar obrigatoriamente.
-		 * Crie um programa que receba a idade dos passageiros em potencial e indique se podem embarcar e votar.*/
+		 * 
+		 * Para evitar problemas no momento do embarque, porem, voce foi convidado a criar um programa que valide a idade dos passageiros: 
+		 * 
+		 * caso tenham menos de 16 anos, nao podem votar (e nem embarcar)
+		 * caso tenham entre 16 anos e 18 incompletos, podem optar por votar ou nao
+		 * caso tenham mais de 18 anos, devem votar obrigatoriamente
+		 * 
+		 * Crie um programa que receba a idade dos passageiros em potencial e indique se podem embarcar e votar.
+		 * */
 		
 		Scanner leitor = new Scanner(System.in);
 		int idade;
+		
 		System.out.println("Por favor, digite a idade do passageiro.");
 		idade = leitor.nextInt();
 	
